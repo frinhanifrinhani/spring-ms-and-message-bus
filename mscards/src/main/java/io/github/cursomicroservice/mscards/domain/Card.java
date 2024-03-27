b5be21ca4b7bd6b771306fa3a0d1872ee7931361
Thiago Frinhani <thiago.frinhani@ths.inf.br>
2024-03-27 19:56:13 -0300

    Recurso buscando cartoes para renda até

--- mscards/src/main/java/io/github/cursomicroservice/mscards/domain/Card.java
+++ mscards/src/main/java/io/github/cursomicroservice/mscards/domain/Card.java
@@ -2,6 +2,7 @@ package io.github.cursomicroservice.mscards.domain;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 @Entity
 @Table(name = "CARD")
 @Data
+@RequiredArgsConstructor
 public class Card {
 
     @Id
